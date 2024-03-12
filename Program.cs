e24a1767b11ccb154ff421eadeb8e65011124b70
Dylan <dylan.drumgoole@fenergo.com>
2024-03-12 12:44:21 +0000

    basic del example

--- Program.cs
+++ Program.cs
@@ -5,7 +5,16 @@
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
-          
+            LogDel logDel = new LogDel(LogTextToScreen);
+
+            logDel("text");
+
+            Console.ReadKey();
+        }
+
+        static void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
         }
     }
 
